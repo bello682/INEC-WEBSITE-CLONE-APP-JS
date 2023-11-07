898f9ad4d2d8ec93c35321a478b578ba63ce27ca
bello682 <belloadetayo14@gmail.com>
2023-11-07 17:22:11 +0100

    INEC WEBSITE CLONE APP
    description

---
+++ index.js
@@ -0,0 +1,9 @@
+function runFunction(sugar, milk) {
+	var inStruction = "Boil Water ";
+	inStruction += "Pour into a cup";
+
+	// return inStruction;
+	return inStruction + "" + sugar + milk;
+}
+
+console.log(runFunction(1, 5));
